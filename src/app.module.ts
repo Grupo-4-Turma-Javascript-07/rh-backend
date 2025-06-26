a8153f56634706cd6fc4308117b8550752f4dd16
oligEdu <egarcia_junior@hotmail.com>
2025-06-26 12:42:56 -0300

    Inserção do Módulo Colaborador e Integração ao DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
+import { ColaboradorModule } from './colaborador/colaborador.module';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Colaborador } from './entities/colaborador.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '2224',
+      database: 'db_rh',
+      entities: [],
+      synchronize: true,
+    }),
+    ColaboradorModule,
+  ],
   controllers: [],
   providers: [],
 })
