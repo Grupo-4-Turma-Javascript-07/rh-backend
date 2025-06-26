6cf13b5b4bef2e00054c3a5498eb74b140df15c1
Melissa <melissasilva245@gmail.com>
2025-06-26 13:19:16 -0300

    implementação do entity no projeto

---
+++ src/Colaborador/entities/colabolador.entity.ts
@@ -0,0 +1,25 @@
+import { IsNotEmpty } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsDateString } from 'class-validator';
+
+@Entity({ name: 'tb_colaboradores' })
+export class Colaborador {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  nome: string;
+
+  @IsNotEmpty()
+  @Column({ length: 11, nullable: false })
+  cpf: string;
+
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  setor: string;
+
+  @IsDateString()
+  @Column({ type: 'date', nullable: false })
+  dataAdmissao: Date;
+}
