82a8e50af0718234a6c74f445de2816fd78148c5
larissa <rissapins@gmail.com>
2025-06-26 13:09:34 -0300

    feat: cria controller de Colaborador com métodos CRUD

---
+++ src/controllers/colaborador.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Colaborador } from '../entities/colaborador.entity';
+import { ColaboradorService } from '../services/colaborador.service';
+
+@Controller('/colaboradores')
+export class ColaboradorController {
+  constructor(private readonly colaboradorService: ColaboradorService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Colaborador[]> {
+    return this.colaboradorService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Colaborador> {
+    return this.colaboradorService.findById(id);
+  }
+
+  @Get('/cargo/:cargo')
+  @HttpCode(HttpStatus.OK)
+  findAllByCargo(@Param('cargo') cargo: string): Promise<Colaborador[]> {
+    return this.colaboradorService.findAllByCargo(cargo);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() colaborador: Colaborador): Promise<Colaborador> {
+    return this.colaboradorService.create(colaborador);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() colaborador: Colaborador): Promise<Colaborador> {
+    return this.colaboradorService.update(colaborador);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.colaboradorService.delete(id);
+  }
+}
